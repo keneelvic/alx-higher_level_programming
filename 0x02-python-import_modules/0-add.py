e8df14aa531fd133b603a9f0b90e745406a0f9b4
Keneel <keneelsoundton@gmail.com>
2022-12-01 15:04:19 -0800

    basically importing a module into a file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,6 +3,6 @@ from add_0 import add
 a = 1
 b = 2
 def main():
-    print("1 + 2 = {:d}".format(add(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
 if __name__ == "__main__":
     main()
