beef4cf1f82b30ae929bde355e7c813ecf5f66b0
Keneel <keneelsoundton@gmail.com>
2022-12-01 16:57:20 -0800

    program that prints arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,12 +10,3 @@ if __name__ == "__main__":
         print("{:d} arguments:".format(len(argv) - 1))
         for i in range(1, len(argv)):
             print("{:d}: {:s}".format(i, argv[i]))
-
-        #if len(sys.argv) == 0 and count == 0:
-         #   print("{:d} arguments.".format(len(sys.argv)))
-        #elif len(sys.argv) > 1:
-         #   print("{:d} arguments:".format(len(sys.argv)))
-          #  print("{:d}: {:s}".format(count, x))
-       # else:
-        #    print("{:d} argument:".format(len(sys.argv)))
-         #   print("{:d}: {:s}".format(count, x))
