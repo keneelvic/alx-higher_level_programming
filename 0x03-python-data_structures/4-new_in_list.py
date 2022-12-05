5160664ae594b914a046d60a50b12d932a96f371
Keneel <keneelsoundton@gmail.com>
2022-12-05 14:09:41 -0800

    replacin a value without changing the value of te original list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    news_list = []
     if my_list:
-        if idx >= 0 or idx <= len(my_list):
+        if idx >= 0 and idx < len(my_list):
             news_list = [] + my_list
             news_list[idx] = element
             return news_list
