26f9e42125dc229828f33d46a9594cf3f5e2559b
Keneel <keneelsoundton@gmail.com>
2022-12-06 14:57:33 -0800

    printing the max valie of a list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     max_val = my_list[0]
-    if my_list != []:
+    if len(my_list) > 0:
         for i in my_list[1:]:
             if i > max_val:
                 max_val = i
         return max_val
-    return None
+    else:
+        return None
